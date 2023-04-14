07219fb00b3760ffe350a6378cbe973ead016f33
Nguyễn Viết Hiếu <hieuconstructor@gmail.com>
2023-04-15 00:27:19 +0700

    Get started food delivery app

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: "Việt Nam",
+                        color: AppColors.mainColor,
+                        size: 30,
+                      ),
+                      Text(
+                        "Thai Binh",
+                        style: TextStyle(fontSize: 12),
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
